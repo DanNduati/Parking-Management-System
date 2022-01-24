557698e63b71631745c6b955cf0185e8f77840ae
DanielNduati <dannduati2@gmail.com>
2022-01-25 01:13:33 +0300

    chore: refactor slot generator modules
    Save slot json data

--- tests/slots/coordinates.py
+++ tests/slots/coordinates.py
@@ -1,3 +1,4 @@
+import json
 import cv2
 import numpy as np
 from colours import COL_WHITE
@@ -7,47 +8,48 @@ from drawing_utils import draw_contours
 class CoordinatesGenerator:
     KEY_QUIT = ord("q")
 
-    def __init__(self, image, caption, color) -> None:
-
+    def __init__(self, image, caption, output_file, color):
         self.caption = caption
+        self.output_file = output_file
         self.color = color
-
         self.image = cv2.imread(image).copy()
         self.click_count = 0
         self.ids = 0
         self.coordinates = []
+        self.coordinates_ = []
         cv2.namedWindow(self.caption, cv2.WINDOW_GUI_EXPANDED)
-        cv2.setMouseCallback(self.caption, self.__mouse_callback)
+        cv2.setMouseCallback(self.caption, self.handle_mouse)
 
-    def generate(self) -> None:
+    def generate(self):
         while True:
             cv2.imshow(self.caption, self.image)
             key = cv2.waitKey(0)
-            if key == CoordinatesGenerator.KEY_QUIT:
-                # cv2.imwrite(f"tests/slots/images/slots.jpg", self.image)
+            if key == self.KEY_QUIT:
+                # store coordinates to file
+                self.store_slots(self.coordinates_, self.output_file)
+                cv2.imwrite(f"tests/slots/images/slots.jpg", self.image)
                 break
         cv2.destroyAllWindows()
+        return self.coordinates_
 
-    def __mouse_callback(self, event, x, y, flags, param):
+    # mouse callback function
+    def handle_mouse(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
-            self.coordinates.append((x, y))
+            self.coordinates.append([x, y])
             self.click_count += 1
-
             if self.click_count >= 4:
-                # handle done
-                self.__handle_done()
+                self.handle_done()
             elif self.click_count > 1:
-                # handle slot space drawing progress
-                self.__handle_click_progress()
+                self.handle_click_progressing()
         cv2.imshow(self.caption, self.image)
 
-    def __handle_click_progress(self):
-        # draw line for last 2 cordinate entries
+    def handle_click_progressing(self):
+        # draw line for last 2 coordinate entries
         cv2.line(self.image, self.coordinates[-2],
                  self.coordinates[-1], self.color, 2)
 
-    def __handle_done(self):
-        # draw lines from the third(index 2) cordinate to the last(index 3) and from the first(index 0) to the last(index 3)
+    def handle_done(self) -> list:
+        # draw lines from the third(index 2) coordinate to the last(index 3) and from the first(index 0) to the last(index 3)
         cv2.line(self.image, self.coordinates[2],
                  self.coordinates[3], self.color, 2)
         cv2.line(self.image, self.coordinates[3],
@@ -55,10 +57,34 @@ class CoordinatesGenerator:
         # reset click count
         self.click_count = 0
         coordinates = np.array(self.coordinates)
-        print(coordinates)
-
-        draw_contours(self.image, coordinates, f"P{self.ids}", COL_WHITE)
+        # append coordinates to the coordinates_ list
+        self.coordinates_.append(coordinates.tolist())
+        draw_contours(self.image, coordinates, f"SLOT{self.ids}", COL_WHITE)
         # reset coordinates
         for i in range(len(self.coordinates)):
             self.coordinates.pop()
         self.ids += 1
+
+    def store_slots(self, coordinates, file_):
+        '''
+        # json template of how i want to store slot data
+        slots = {
+            1: {
+                "id": "slot1",
+                "coordinates": [[], [], [], []]
+            },
+            2: {
+                "id": "slot2",
+                "coordinates": [[], [], [], []]
+            }
+        }
+        '''
+        slots = dict()
+        for i, cord in enumerate(coordinates):
+            slots[i] = {
+                "id": f"slot{i}",
+                "coordinates": cord
+            }
+        print(slots)
+        with open(file_, 'w') as f:
+            json.dump(slots, f)
--- tests/slots/main.py
+++ tests/slots/main.py
@@ -1,11 +1,13 @@
-from colours import COL_BLUE, COL_GREEN
+from colours import COL_BLUE
 from coordinates import CoordinatesGenerator
 
 
 def main():
     image_file = "images/kimathi4.png"
+    cord_file = "tests/slots/data/coordinates.json"
     if image_file is not None:
-        generator = CoordinatesGenerator(image_file, 'Slots', COL_BLUE)
+        generator = CoordinatesGenerator(
+            image_file, 'Slots', cord_file, COL_BLUE)
         generator.generate()
 
 
